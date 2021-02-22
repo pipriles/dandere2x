1e9147a6fa5c29ed737975ef4d12705bdbcc0125
tyler <akaikatto@gmail.com>
2021-02-21 17:28:03 -0800

    changes for macos installation

--- src/macos_setup.sh
+++ src/macos_setup.sh
@@ -5,10 +5,10 @@ cd ./src
 # create the externals folder and move over the recently made d2x_cpp binary
 mkdir -p externals
 
-wget https://github.com/akai-katto/dandere2x_externals_static/releases/download/0.0.0/dandere2x_cpp -p externals
 
 # download waifu2x-ncnn-vulkan and extract it the way d2x expects it
 cd externals
+wget https://github.com/akai-katto/dandere2x_externals_static/releases/download/0.0.0/dandere2x_cpp
 wget https://github.com/nihui/waifu2x-ncnn-vulkan/releases/download/20210210/waifu2x-ncnn-vulkan-20210210-macos.zip
 unzip waifu2x-ncnn-vulkan-20210210-macos.zip
 mv waifu2x-ncnn-vulkan-20210210-macos ./waifu2x-ncnn-vulkan
